6891a226f646e5d381c9639af511ccac2acd89cf
Sureshkumar Linganathan <sureshkumarlinganathan@suresh-kumar.local>
2018-06-18 15:58:09 +0530

    Initial Commit

---
+++ HackerNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HackerNews
+//
+//  Created by Sureshkumar Linganathan on 6/18/18.
+//  Copyright © 2018 Omnify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HackerNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HackerNews
+//
+//  Created by Sureshkumar Linganathan on 6/18/18.
+//  Copyright © 2018 Omnify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
